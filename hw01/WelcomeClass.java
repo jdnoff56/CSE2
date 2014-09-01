c94696a5b5bf9e2c5d8b138b5988130fba138976
jdnoff56 <jad217@lehigh.edu>
2014-09-01 00:01:39 +0000

    hw01-WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Joseph D'Onofrio
+//CSE2
+//hw01-WelcomeClass program
+
+
+//Define Class
+public class WelcomeClass{
+
+    //Add Main Method
+    public static void main(String[] args) {
+        
+        // Display text
+        System.out.println("   -----------    ");
+        System.out.println("   | WELCOME |    ");
+        System.out.println("   -----------    ");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-J--A--D--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("This summer i spent 10 days in Copenhagen, Denmark");
+        System.out.println("with my family.");
+    }
+}
